751420409979ff6b44a724675b2c8e08d2a142b0
lxzemer <609650073@qq.com>
2019-11-27 18:21:10 +0800

    lala

--- src/view/Account/AccountList.vue
+++ src/view/Account/AccountList.vue
@@ -10,6 +10,7 @@
                         class="input-class"
                         v-model="searchInfo.payDate"
                         type="date"
+                        value-format="yyyy-MM-dd"
                         placeholder="选择日期">
                 </el-date-picker>
             </FormItem>
